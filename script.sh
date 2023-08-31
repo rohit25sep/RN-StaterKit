38d69f7eb37b7ff7d7fac8e5447d7b343fe32527
Rohit Kumar <rohit@rohitss596.local>
2023-08-31 17:54:37 +0530

    auto ios

--- script.sh
+++ script.sh
@@ -10,4 +10,4 @@ echo "\nInstalling node_modules\n"
 npm install
 
 echo "\nInitializing husky hooks\n"
-npx husky set .husky/pre-commit "npm run test && npm run lint"
+npx husky set .husky/pre-commit "npm run lint"
