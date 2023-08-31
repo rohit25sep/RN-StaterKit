c9bc7e2ce299bf2063bb20c4fc6f3ff6dc38f257
Rohit Kumar <rohit@rohitss596.local>
2023-08-31 17:09:35 +0530

    test: ios build

--- script.sh
+++ script.sh
@@ -3,11 +3,7 @@
 echo "\nInitializing git repository\n"
 git init
 
-echo "\nInitializing husky (Please hit return/enter key for the next prompt)\n"
-npx husky-init
 
 echo "\nInstalling node_modules\n"
 npm install
 
-echo "\nInitializing husky hooks\n"
-npx husky set .husky/pre-commit "npm run test && npm run lint"
